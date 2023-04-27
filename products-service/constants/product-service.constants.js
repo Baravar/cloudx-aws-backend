7711d1bfa0228dea8868c349e15bc421814b765f
Artsiom Baravik <artsiom.baravik@aerlingus.com>
2023-04-27 22:56:27 +0300

    task-6: setup SNS and integrate it with SQS handler

--- products-service/constants/product-service.constants.js
+++ products-service/constants/product-service.constants.js
@@ -1,1 +1,2 @@
 export const dynamoDbRegion = 'us-east-1';
+export const snsRegion = 'us-east-1';
--- products-service/handlers/catalogBatchProcess.js
+++ products-service/handlers/catalogBatchProcess.js
@@ -1,9 +1,37 @@
+import { PublishCommand } from '@aws-sdk/client-sns';
+import { snsClient } from '../services/sns/sns-client.js';
+
+const TOPIC_ARN = process.env.SNS_ARN; 
+
 export const catalogBatchProcess = async event => {
-  console.log(event);
-  console.log('QUEUE_URL: ', process.env.QUEUE_URL);
+  console.log('SQS Event: ', event);
+  const processingPromises = [];
+
   event.Records.forEach(record => {
-    console.log(record);
+    console.log('Record to handle: ', record);
+
+    const params = {
+      Subject: 'New Products have been added to the Store DB',
+      Message: record.body,
+      TopicArn: TOPIC_ARN
+    };
+
+    processingPromises.push(new Promise(async resolve => {
+      try {
+        console.log('SNS Push Params: ', params);
+        const data = await snsClient.send(new PublishCommand(params));
+        console.log('Success.', data);
+        resolve(data);
+      } catch (err) {
+        console.log('Error', err.stack);
+      }
+    }));
+    
   });
+
+  return Promise.all(processingPromises).then(values => {
+    console.log('Processed SNS events: ', values);
+  });;
 };
 
 export default catalogBatchProcess;
