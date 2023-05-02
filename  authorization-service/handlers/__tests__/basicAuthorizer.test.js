50d13d60156606e265277663781c46b5af746727
Artsiom Baravik <artsiom.baravik@aerlingus.com>
2023-05-02 23:24:19 +0300

    task-7: setup initial Auth Lambda placeholder and integrate it with Import API

---
+++  authorization-service/handlers/__tests__/basicAuthorizer.test.js
@@ -0,0 +1,8 @@
+import { basicAuthorizer } from '../basicAuthorizer.js';
+
+describe('basicAuthorizer', () => {
+  it('...', async () => {
+    const eventMock = {};
+    await expect(basicAuthorizer(eventMock)).resolves.toBe(undefined);
+  });
+});
